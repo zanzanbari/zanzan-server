5903495510574357aef619882ae6753772c6d0a0
chanwoo <chocw0402@gmail.com>
2022-02-13 00:12:33 +0900

    [CHORE]

--- functions/src/service/authService.js
+++ functions/src/service/authService.js
@@ -159,38 +159,4 @@ module.exports = {
             return -3;
         }
     },
-
-    /**             accesstokenDecode = jwtHandler.verifyToken(accesstoken);
-
-            if (accesstokenDecode === TOKEN_EXPIRED || accesstokenDecode === TOKEN_INVALID) {
-                refreshtokenDecode = jwtHandler.verifyToken(refreshtoken);
-
-                if (refreshtokenDecode === TOKEN_EXPIRED || refreshtokenDecode === TOKEN_INVALID) {
-                    return res.send(util.fail(401, '토큰이 만료되었습니다. 다시 로그인 하세요.'));
-                } else {
-                    const user = await User.findOne({ where: { refreshtoken } });
-                    // accesstoken 재발급
-                    const { accesstoken: newAccesstoken } = jwtHandler.issueAccessToken(user);
-                    // 클라에 넘겨줄 엑세스 토큰
-                    res.cookie('accesstoken', newAccesstoken);
-                    req.cookies.accesstoken = newAccesstoken;
-                    req.user = user;
-                    next();
-                }
-                
-            } else if (refreshtokenDecode === TOKEN_EXPIRED || refreshtokenDecode === TOKEN_INVALID) {
-                const idFirebase = accesstokenDecode.idFirebase;
-                const user = await User.findOne({ where: { idFirebase } });
-                const { refreshtoken: newRefreshtoken } = jwtHandler.issueRefreshToken();
-                // 클라에 넘겨줄 리프레쉬 토큰
-                res.cookie('refreshtoken', newRefreshtoken);
-                req.cookies.refreshtoken = newRefreshtoken;
-                req.user = user;
-                next();
-            } else {
-                const idFirebase = accesstokenDecode.idFirebase;
-                const user = await User.findOne({ where: { idFirebase } });
-                req.user = user;
-                next();
-            } */
 }
