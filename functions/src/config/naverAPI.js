8f0b165f32de1310be9f0c638be8bee0117741ed
chanwoo <chocw0402@gmail.com>
2022-02-13 18:37:00 +0900

    [ADD] 네이버 발급 키

---
+++ functions/src/config/naverAPI.js
@@ -0,0 +1,7 @@
+const naver_client_id = process.env.NAVER_CLIENT_ID;
+const naver_client_secret = process.env.NAVER_CLIENT_SECRETE;
+
+module.exports = {
+    naver_client_id,
+    naver_client_secret,
+};
