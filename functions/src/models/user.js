33272df5af3d0b524a000af8b0befe13776e6c08
chanwoo <chocw0402@gmail.com>
2022-02-14 05:16:40 +0900

    [FIX] DB 모델 수정

---
+++ functions/src/models/user.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) { // 테이블 컬럼 설정
+        return super.init({
+            social: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+                defaultValue: 'local',
+            },
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING(255),
+                allowNull: false,
+                // set: async function (pwd) {
+                //     try {
+                //         const salt = await bcrypt.genSalt(10);
+                //         const hashedPassword = await bcrypt.hash(pwd, salt);
+                //         this.setDataValue('password', hashedPassword);
+                //     } catch (error) {
+                //         console.log('❌ HASHING ERROR');
+                //     }
+                // },
+            },
+            nickname: {
+                type: Sequelize.STRING(30),
+                allowNull: false,
+            },
+            refreshtoken: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            }
+        }, { // 테이블 자체 설정
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: true,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+
+    static associate(db) {}
+}
