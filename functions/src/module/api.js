0246a726db44d7985f2f8627b8883b3b1f17c6dd
chanwoo <chocw0402@gmail.com>
2022-02-17 01:03:29 +0900

    [FIX]

--- functions/src/module/api.js
+++ functions/src/module/api.js
@@ -5,16 +5,25 @@ const { naver_client_id, naver_client_secret } = require('../config/naverAPI');
 module.exports = {
     getNaverTokenByCodeAPI: async (code) => {
         try {
-            const api_url = 
-                `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code` +
-                `&client_id=${naver_client_id}` + 
-                `&client_secret=${naver_client_secret}` +
-                `&code=${code}`;
-            
-            const tokenData = await axios.get(api_url);
-            console.log('🚀🚀🚀 ', tokenData);
-            
-            return tokenData;
+            const baseUrl = 'https://nid.naver.com/oauth2.0/token';
+            const config = {
+                grant_type: 'authorization_code',
+                client_id: naver_client_id,
+                client_secret: naver_client_secret,
+                code: code,
+            };
+            const params = new URLSearchParams(config).toString();
+            const finalUrl = `${baseUrl}?${params}`;
+            console.log('📌',finalUrl);
+            const tokenData = await axios.get(finalUrl);
+            console.log('📌📌',tokenData);
+            if ('access_token' in tokenData) {
+                const { access_token } = tokenData;
+                return access_token;
+            } else {
+                const err = new Error('access_token 가져오기 실패');
+                return err;
+            }
         } catch (error) {
             console.log('❌ Naver request API Error: ', error);
             return new Error(error);
@@ -23,17 +32,17 @@ module.exports = {
 
     NaverAuthAPI: async (naverAccessToken) => {
         try {
-            const verifyUser = await axios({
-                method: 'GET',
-                url: 'https://openapi.naver.com/v1/nid/verify',
-                body: {
-                    info: true,
-                },
-                Headers: {
-                    Authorization: `Bearer ${naverAccessToken}`,
-                },
-            });
-            console.log('🚀🚀🚀 ', verifyUser);
+            // const verifyUser = await axios({
+            //     method: 'GET',
+            //     url: 'https://openapi.naver.com/v1/nid/verify',
+            //     body: {
+            //         info: true,
+            //     },
+            //     Headers: {
+            //         Authorization: `Bearer ${naverAccessToken}`,
+            //     },
+            // });
+            // console.log('🚀🚀🚀 verifyUser: ', verifyUser);
 
             const user = await axios({
                 method: 'GET',
@@ -45,7 +54,7 @@ module.exports = {
 
             const naverUser = user.data.response;
             // TODO: 에러처리
-            console.log('🐯🐯🐯 ', naverUser);
+            console.log('🐯🐯🐯 naverUser:', naverUser);
 
             return naverUser;
         } catch (error) {
