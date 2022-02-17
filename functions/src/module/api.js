34a858b629e63d8a32fa491e34bf5166bed17ecd
chanwoo <chocw0402@gmail.com>
2022-02-18 04:52:40 +0900

    [REMOVE] getNaverTokenByCodeAndStateAPI 모듈 제거(불필요 모듈)

--- functions/src/module/api.js
+++ functions/src/module/api.js
@@ -4,43 +4,6 @@ const naver_client_id = process.env.NAVER_CLIENT_ID;
 const naver_client_secret = process.env.NAVER_CLIENT_SECRETE;
 
 module.exports = {
-    getNaverTokenByCodeAndStateAPI: async (code, state) => {
-        try {
-            const baseUrl = 'https://nid.naver.com/oauth2.0/token';
-            const config = {
-                grant_type: 'authorization_code',
-                client_id: naver_client_id,
-                client_secret: naver_client_secret,
-                code: code,
-                state: state,
-            };
-            const queryString = qs.stringify(config);
-            const finalUrl = `${baseUrl}?${queryString}`;
-            const tokenData = await (
-                await fetch(finalUrl, {
-                    method: 'POST',
-                    headers: {
-                        'X-Naver-Client-Id': naver_client_id,
-                        'X-Naver-Client-Secret': naver_client_secret
-                    },
-                })
-            ).json();
-            if ('error' in tokenData) {
-                let { error, error_description } = tokenData;
-                return new Error({ error, error_description });
-            }
-            const { access_token, refresh_token } = tokenData;
-            const result = {
-                access_token,
-                refresh_token,
-            };
-            return result;
-        } catch (error) {
-            console.log('❌ Naver request API Error: ', error);
-            return new Error(error);
-        }
-    },
-
     NaverAuthAPI: async (naverAccessToken) => {
         try {
             const apiUrl = 'https://openapi.naver.com/v1/nid/me';
